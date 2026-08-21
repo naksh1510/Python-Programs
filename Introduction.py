6bfcc2f845c5971516b37958b3f514de86c49cb2
naksh1510 <naksh.nampally@hotmail.com>
2026-08-21 17:16:49 +0100

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,26 @@
+#Printing a greeting message
+print("Hello,world!")
+
+# Printing my name
+print("My name is Naksh")
+
+# Printing a fun fact about myself
+# print("I love coding and solving problems!")
+print("I love coding and solving problems!")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
+#Print a sentence
+print("Welcome to the world of programming!")
+
+# Print a number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Print all the keywords present in python
+print("The keywords in Python are...")
+import keyword
+print(keyword.kwlist)
