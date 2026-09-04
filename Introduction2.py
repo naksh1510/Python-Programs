346881614011578021474dd404b2161c5cd6152b
naksh1510 <naksh.nampally@hotmail.com>
2026-09-04 17:07:31 +0100

    Add files via upload

---
+++ Introduction2.py
@@ -0,0 +1,11 @@
+def intro(name):
+    print("Hello, Good morning! I am",name)
+
+user_name = input("Enter your name: ")
+intro(user_name)
+
+def fav_color(color):
+    print("My favourite color is",color)
+
+user_color = input("Enter your favourite color: ")
+fav_color(user_color)
